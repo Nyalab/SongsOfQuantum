a324640a91d4842b081328f139069c9793d10733
Tristan Maindron <tmaindron@gmail.com>
2012-05-08 23:59:23 +0200

    An asteroid can now be gathered by only one miner

--- js/classes/bodies/asteroid.class.js
+++ js/classes/bodies/asteroid.class.js
@@ -1,9 +1,29 @@
 var Asteroid = Entity.extend({
     __construct: function(id, x, y){
-        this._super(id, 0, x, y, 'images/bodies/asteroid.png', 300);
+        this._super(id, 0, x, y, 'images/bodies/asteroid.png', 50);
         this.flag('asteroid');
         this.properties = {
         	name: "Asteroid"
         }
+        this.currentMiner = null;
+        this.free();
+    },
+
+    isFree: function(){
+    	return this.currentMiner == null;
+    },
+
+    getMiner: function(){
+    	return this.currentMiner;
+    },
+
+    occupy: function(miner){
+    	this.unflag('free');
+    	this.currentMiner = miner;
+    },
+
+    free: function(){
+    	this.flag('free');
+    	this.currentMiner = null;
     }
 });
